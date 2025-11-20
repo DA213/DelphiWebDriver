e2b07d7efad5a05f7095d9005fefeef955835508
ABDERRAHMANE <abderrahmane.job@gmail.com>
2025-11-19 17:55:16 -0800

    Update [TWebDriverServer]

--- Source/DelphiWebDriver.Server.pas
+++ Source/DelphiWebDriver.Server.pas
@@ -8,12 +8,12 @@ uses
   , Posix.Unistd
   , Posix.SysTypes
   , Posix.SysWait
-  , Posix.Spawn
   , Posix.Signal
 {$ENDIF}
 {$IFDEF MSWINDOWS}
-  , Winapi.Windows;
+  , Winapi.Windows
 {$ENDIF}
+  ;
 
 type
   TWebDriverServer = class
@@ -55,35 +55,51 @@ begin
 end;
 
 procedure TWebDriverServer.Start;
+{$IFDEF POSIX}
+var
+  PID: pid_t;
+  ArgV: array[0..2] of PAnsiChar;
+{$ENDIF}
 var
   Cmd: string;
-{$IFDEF POSIX}
-  Args: array[0..2] of PAnsiChar;
-  Status: Integer;
-{$ENDIF}
 begin
   if FStarted then
     Exit;
+
   if not FileExists(FExePath) then
     raise Exception.Create('WebDriver executable not found: ' + FExePath);
+
   Cmd := FExePath + ' --port=9515';
+
   {$IFDEF MSWINDOWS}
-  var Startup: TStartupInfo;
-  ZeroMemory(@Startup, SizeOf(Startup));
+  var SI: TStartupInfo;
+  ZeroMemory(@SI, SizeOf(SI));
   ZeroMemory(@FProcessInfo, SizeOf(FProcessInfo));
-  Startup.cb := SizeOf(Startup);
-  if not CreateProcess(nil, PChar(Cmd), nil, nil, False, CREATE_NO_WINDOW, nil, nil,
-                      Startup, FProcessInfo) then
+  SI.cb := SizeOf(SI);
+
+  if not CreateProcess(nil, PChar(Cmd), nil, nil, False, CREATE_NO_WINDOW,
+                      nil, nil, SI, FProcessInfo) then
     raise Exception.Create('Cannot start driver: ' + SysErrorMessage(GetLastError));
   {$ENDIF}
+
   {$IFDEF POSIX}
-  Args[0] := PAnsiChar(AnsiString(FExePath));
-  Args[1] := PAnsiChar(AnsiString('--port=9515'));
-  Args[2] := nil;
-  Status := posix_spawn(@FPID, PAnsiChar(AnsiString(FExePath)), nil, nil, @Args[0], environ);
-  if Status <> 0 then
-    raise Exception.Create('Failed to start WebDriver process (posix_spawn), errno=' + Status.ToString);
+  ArgV[0] := PAnsiChar(AnsiString(FExePath));
+  ArgV[1] := PAnsiChar(AnsiString('--port=9515'));
+  ArgV[2] := nil;
+
+  PID := fork;
+  if PID = -1 then
+    raise Exception.Create('fork() failed');
+
+  if PID = 0 then
+  begin
+    execvp(ArgV[0], @ArgV[0]);
+    _exit(127);
+  end;
+
+  FPID := PID;
   {$ENDIF}
+
   FStarted := True;
   Sleep(700);
 end;
@@ -92,28 +108,37 @@ procedure TWebDriverServer.Stop;
 begin
   if not FStarted then
     Exit;
+
   {$IFDEF MSWINDOWS}
   if FProcessInfo.hProcess <> 0 then
   begin
     if WaitForSingleObject(FProcessInfo.hProcess, 1500) = WAIT_TIMEOUT then
       TerminateProcess(FProcessInfo.hProcess, 0);
+
     WaitForSingleObject(FProcessInfo.hProcess, 500);
     CloseHandle(FProcessInfo.hProcess);
     CloseHandle(FProcessInfo.hThread);
+
     FProcessInfo.hProcess := 0;
     FProcessInfo.hThread := 0;
   end;
   {$ENDIF}
+
   {$IFDEF POSIX}
   if FPID > 0 then
   begin
-    fpKill(FPID, SIGTERM);
+    kill(FPID, SIGTERM);
     Sleep(300);
-    if fpKill(FPID, 0) = 0 then
-      fpKill(FPID, SIGKILL);
+
+    if kill(FPID, 0) = 0 then
+      kill(FPID, SIGKILL);
+
+    waitpid(FPID, nil, 0);
+
     FPID := 0;
   end;
   {$ENDIF}
+
   FStarted := False;
 end;
 
